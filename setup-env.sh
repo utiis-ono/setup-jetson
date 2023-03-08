1512a68a7ec73f13eef376d8f09b362e04fbd60e
utiis-ono <1698122021@edu.k.u-tokyo.ac.jp>
2023-03-08 18:47:48 +0900

    [update] setup-env

--- setup-env.sh
+++ setup-env.sh
@@ -16,6 +16,17 @@ sudo apt-get -y upgrade
 #Dependencies
 sudo apt-get -y install python3-setuptools
 
+#python3.9 install
+wget https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tar.xz
+tar xJf Python-3.9.2.tar.xz
+cd Python-3.9.2
+./configure --enable-optimizations
+make
+sudo make install
+
+sudo pip install --upgrade pip
+
+
 #Installing PyTorch
 #For latest PyTorch refer original Nvidia Jetson Nano thread - https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/.
 wget https://nvidia.box.com/shared/static/yr6sjswn25z7oankw8zy1roow9cy5ur1.whl -O torch-1.6.0rc2-cp36-cp36m-linux_aarch64.whl
@@ -25,9 +36,11 @@ sudo apt-get -y install python3-pip libopenblas-base
 sudo pip3 install Cython numpy
 sudo pip3 install torch-1.6.0rc2-cp36-cp36m-linux_aarch64.whl
 
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+
 #Installing torchvision
 #For latest torchvision refer original Nvidia Jetson Nano thread - https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/.
-sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev
+sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev libbz2-dev
 sudo apt-get -y install libopenblas-base libopenmpi-dev
 sudo apt-get -y install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
 #git clone --branch v0.7.0 https://github.com/pytorch/vision torchvision   # see below for version of torchvision to download
@@ -72,5 +85,26 @@ cd text
 sudo pip3 install -r requirements.txt
 sudo python3 setup.py install
 cd ../
+
+pyenv install 3.9.2
+pyenv local 3.9.2
+pyenv global 3.9.2
+
+#モジュールインストール
+python3 -m pip install numpy
+python3 -m pip install typing_extensions
+python3 -m pip install requests
+python3 -m pip install tqdm
+
+#cudaインストール
+sudo apt purge -y nvidia-*
+sudo apt purge -y cuda-*
+sudo apt autoremove -y --purge
+
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+#sudo dpkg -i cuda-keyring_1.0-1_all.deb
+#sudo apt-get -y update
+#sudo apt install -y cuda-11-7
+
 
 echo "done installing PyTorch, torchvision, spaCy, torchtext"
